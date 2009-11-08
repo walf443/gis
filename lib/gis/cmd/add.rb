ee8942c7980bdfabf9c14c0d28a010a19789005e
Keiji, Yoshimi <walf443@gmail.com>
2009-11-08 16:59:16 +0900

    ちょっと仕様を代えて、GitStoreを使わずに別ブランチにファイルとして保持するようにした

--- lib/gis/cmd/add.rb
+++ lib/gis/cmd/add.rb
@@ -1,34 +1,38 @@
 require 'tempfile'
 require 'digest/md5'
+require 'fileutils'
 
 class Gis
     module Cmd
         class Add
             def initialize app, args, options
                 @app = app
-                @store = Gis::Store.new(@app.git_repos.to_s)
+                @branch = args.shift or 
+                    raise "branch is missing"
             end
 
             def run
-                t = Tempfile.new('gis' + @app.git_repos.to_s)
-                editor = ENV['EDITOR']
-                system(editor, t.path)
-                digest = nil
-                commit = nil
-                content = nil
-                File.open(t.path) do |file|
-                    content = file.read
-                    digest = Digest::MD5.hexdigest(content)
-                    commit = @store[digest] = content
-                end
-
-                if commit
-                    first_line = nil
-                    content.each_line do |line|
-                        first_line = line
-                        break
+                @app.move_branch(@branch) do
+                    t = Tempfile.new('gis' + @app.git_repos.to_s)
+                    editor = ENV['EDITOR']
+                    system(editor, t.path)
+                    digest = nil
+                    content = nil
+                    File.open(t.path) do |file|
+                        content = file.read
+                        digest = Digest::MD5.hexdigest(content)
                     end
-                    @store.commit "add #{digest}: #{first_line}"
+
+                    if content
+                        first_line = nil
+                        content.each_line do |line|
+                            first_line = line
+                            break
+                        end
+                        FileUtils.cp(t.path, digest)
+                        @app.run_git(:add, digest)
+                        @app.run_git(:commit, %{--message "add #{digest}: #{first_line}"}, digest)
+                    end
                 end
             end
         end
--- lib/gis/cmd/edit.rb
+++ lib/gis/cmd/edit.rb
@@ -6,35 +6,33 @@ class Gis
         class Edit
             def initialize app, args, options
                 @app = app
+                @branch = args.shift or
+                    raise ArgumentError
                 @key = args.shift or
                     raise ArgumentError
 
-                @store = Gis::Store.new(@app.git_repos.to_s)
             end
 
             def run
-                t = Tempfile.new('gis' + @app.git_repos.to_s)
-                editor = ENV['EDITOR']
-                content = @store[@key] or 
-                    raise "no such content: #@key"
-                File.open(t.path, 'w') do |file|
-                    file.write(content)
-                end
-                system(editor, t.path)
-                digest = nil
-                commit = nil
-                File.open(t.path) do |file|
-                    content = file.read
-                    commit = @store[@key] = content
-                end
-
-                if commit
-                    first_line = nil
-                    content.each_line do |line|
-                        first_line = line
-                        break
+                @app.move_branch @branch do
+                    editor = ENV['EDITOR']
+                    unless File.exist?(@key)
+                        raise "no such content: #@key"
                     end
-                    @store.commit "edit #{@key}: #{first_line}"
+                    system(editor, @key)
+                    content = nil
+                    File.open(@key) do |file|
+                        content = file.read
+                    end
+
+                    if content
+                        first_line = nil
+                        content.each_line do |line|
+                            first_line = line
+                            break
+                        end
+                        @app.run_git(:commit, %{--message "edit #{@key}: #{first_line}"}, @key)
+                    end
                 end
             end
         end
